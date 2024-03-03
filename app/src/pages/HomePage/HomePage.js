a986728c4b9a888b87677214d0ee51bc484ea8b7
Yuxuan Shi <1051268839@qq.com>
2024-03-03 17:41:48 -0500

    Update HomePage.js

--- app/src/pages/HomePage/HomePage.js
+++ app/src/pages/HomePage/HomePage.js
@@ -21,15 +21,36 @@ export default function HomePage() {
         >
           <g transform="translate(200,200) rotate(30)">
             <circle cx="0" cy="0" r="215" fill="black" />
-            <path d="M0,-200A200,200 0 0,1 173.2,-100L0,0Z" fill="#80f155" />
-            <path d="M173.2,-100A200,200 0 0,1 173.2,100L0,0Z" fill="black" />
-            <path d="M173.2,100A200,200 0 0,1 0,200L0,0Z" fill="#80f155" />
-            <path d="M0,200A200,200 0 0,1 -173.2,100L0,0Z" fill="black" />
             <path
+              id={1}
+              d="M0,-200A200,200 0 0,1 173.2,-100L0,0Z"
+              fill="#80f155"
+            />
+            <path
+              id={2}
+              d="M173.2,-100A200,200 0 0,1 173.2,100L0,0Z"
+              fill="black"
+            />
+            <path
+              id={3}
+              d="M173.2,100A200,200 0 0,1 0,200L0,0Z"
+              fill="#80f155"
+            />
+            <path
+              id={4}
+              d="M0,200A200,200 0 0,1 -173.2,100L0,0Z"
+              fill="black"
+            />
+            <path
+              id={5}
               d="M-173.2,100A200,200 0 0,1 -173.2,-100L0,0Z"
               fill="#80f155"
             />
-            <path d="M-173.2,-100A200,200 0 0,1 0,-200L0,0Z" fill="black" />
+            <path
+              id={6}
+              d="M-173.2,-100A200,200 0 0,1 0,-200L0,0Z"
+              fill="black"
+            />
             <circle cx="0" cy="0" r="60" fill="black" />
             <circle cx="0" cy="0" r="40" fill="#80f155" />
           </g>
